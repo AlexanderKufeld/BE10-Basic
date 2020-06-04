fca37d19949dd5ce4b688aa17c73f83bde5fe82c
AlexanderKufeld <alexander.kufeld@gmail.com>
2020-06-04 16:48:31 +0200

    Functional Interface

---
+++ Functional_Interface/src/Transformer.java
@@ -0,0 +1,14 @@
+
+import java.util.function.*;
+public class Transformer {
+
+    public static String transform (String text, Predicate<String> p, Function<String, String> f) {
+        String[] words = text.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            if (p.test(words[i])) {
+                words[i] = f.apply(words[i]);
+            }
+        }
+        return String.join(" ", words);
+    }
+}
